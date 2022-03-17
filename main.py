ac7752b2d2067ad1e2263e353395998d09333877
martijn <martijndenhoed17@gmail.com>
2022-03-17 17:35:15 +0100

    Changes by shawn

--- main.py
+++ main.py
@@ -28,11 +28,21 @@ class Propeller:
     #propeller properties
     def B_D(self,z): #chord to diameter ratio
         r_o = z-0.2
-        if(r_o<0.1):
-            return 0
-        B_D_o = 0.127 + 0.7404112848810135 * (r_o ** 1) + -4.673821287763687 * (r_o ** 2) + 19.02690171288593 * (r_o ** 3) + -48.12645454430276 * (r_o ** 4) + 36.20381779945387 * (r_o ** 5) + 111.0096041870429 * (r_o ** 6) + -311.5856431939494 * (r_o ** 7) + 309.1780229926285 * (r_o ** 8) + -113.64512633245806 * (r_o ** 9)
-        B_D = 0.5 * (B_D_o + 0.05)
-        return B_D
+
+        B_D_o=[]
+        if isinstance(r_o,float):
+            i=r_o
+            B_D_o=0.127 + 0.7404112848810135 * (i ** 1) + -4.673821287763687 * (i ** 2) + 19.02690171288593 * (i ** 3) + -48.12645454430276 * (i ** 4) + 36.20381779945387 * (i ** 5) + 111.0096041870429 * (i ** 6) + -311.5856431939494 * (i ** 7) + 309.1780229926285 * (i ** 8) + -113.64512633245806 * (i ** 9)
+            return 0.5 * (B_D_o + 0.05)
+        else:
+            B_D_o = []
+            for i in r_o:
+                if(i<0.1):
+                    B_D_o.append(0)
+                else:
+                    B_D_o.append(0.127 + 0.7404112848810135 * (i ** 1) + -4.673821287763687 * (i ** 2) + 19.02690171288593 * (i ** 3) + -48.12645454430276 * (i ** 4) + 36.20381779945387 * (i ** 5) + 111.0096041870429 * (i ** 6) + -311.5856431939494 * (i ** 7) + 309.1780229926285 * (i ** 8) + -113.64512633245806 * (i ** 9))
+            B_D = 0.5 * (np.array(B_D_o) + 0.05)
+            return B_D
 
     def chord(self, z):
         c = self.B_D(z) * self.diameter
@@ -75,7 +85,7 @@ class Propeller:
     def psi_V(self,step, z,m):
         z_arr = np.arange(-0.5, 0.5, step)
         derivativepsi_V = self.psi_V_derivative(z_arr, [z,m])
-        return Math2.integration(step, derivativepsi_V, 'Simpsons')
+        return math2.integration(step=step,array= derivativepsi_V,type=1)
 
 
      #method 2
@@ -102,33 +112,89 @@ class Propeller:
         z = args[0]
         m = args[1]
         k_x = self.k_x(z, m)
-        Lift_sec = self.CLfunction(x) * 0.5 * self.flow.density * (self.M_r(x) * self.flow.a_0) ** 2 * self.chord(z)
+        Lift_sec = self.CLfunction(x+0.5) * 0.5 * self.flow.density * (self.M_r(x) * self.flow.a_0) ** 2 * self.chord(z)
         return Lift_sec * np.exp(1j*k_x*x)
 
     def psi_V_derivative(self,x,args):
         z = args[0]
         m = args[1]
         k_x = self.k_x(z, m)
-        return self.thickness(x) * np.exp(1j*k_x*x)
+        return self.thickness(x+0.5) * np.exp(1j*k_x*x)
 
 
 
-    def CLfunction(self,x):
+    #p(t)
+    def pressure(self,m,method):
+        k = self.calcFirstPart(m)
+        if method==0:
+            step=0.01
+            z=np.arange(0,1,step)
+            form=0
+            type=1
+            deriV=self.p_Vm_derivative(z,m)[form]
+            deriD=self.p_Dm_derivative(z,m)[form]
+            deriL=self.p_Lm_derivative(z,m)[form]
+            p_Vm = k * math2.integration(step, deriV, type)
+            p_Dm = k * math2.integration(step, deriD, type)
+            p_Lm = k * math2.integration(step, deriL, type)
+            #print(p_Dm,p_Lm,p_Vm)
+        else:
+            p_Vm = k*math2.integrateRiemannSums(self.p_Vm_derivative,0,1,100,m,method)
+            p_Dm = k*math2.integrateRiemannSums(self.p_Dm_derivative,0,1,100,m,method)
+            p_Lm = k*math2.integrateRiemannSums(self.p_Lm_derivative,0,1,100,m,method)
 
-        return 0
 
-    def CDfunction(self,x):
+        p_mb = p_Vm + p_Dm + p_Lm
+        return p_mb
 
-        return 0
+
+    #harmonic noise level
+    def noise(self,m,method):
+        p = self.pressure(m,method)*2
+        print(p)
+        p_ref = 20 * 10**-6
+        OASPL = 10 * math.log10(p / (p_ref))
+        return OASPL
+
+    def p_Vm_derivative(self,z,m):
+        k_x = self.k_x(z, m)
+        P_Vm_1 = (self.M_r(z)) ** 2 * math2.besselsFunc(m*self.bladeNumber, m * self.bladeNumber * z * self.M_t*np.sin(self.theta)/(1-self.flow.M_x*np.cos(self.theta)))
+        P_Vm_2 = self.k_x(z,m)**2*self.thickness(z)*self.psi_V(0.01,z,m)
+        P_Vm_2_alt = self.k_x(z,m)**2*self.thickness(z)*self.psi_V2(z,m)
+
+        return (P_Vm_1*P_Vm_2,P_Vm_1*P_Vm_2_alt)
+
+    def p_Dm_derivative(self, z,m):
+        k_x = self.k_x(z,m)
+        C_D = self.CDfunction(z)
+        P_Dm_1 = (self.M_r(z)) ** 2 * math2.besselsFunc(m*self.bladeNumber, m*self.bladeNumber * z * self.M_t*np.sin(self.theta)/(1-self.flow.M_x*np.cos(self.theta)))
+        P_Dm_2 = 1j * k_x * (C_D/2) * self.psi_D(0.01,z,m)
+        P_Dm_2_alt = 1j * k_x * (C_D/2) * self.psi_D2(z,m)
+        return (P_Dm_1*P_Dm_2,P_Dm_1*P_Dm_2_alt)
+
+    def p_Lm_derivative(self, z,m):
+        k_x = self.k_x(z, m)
+        C_L = self.CDfunction(z)
+        P_Lm_1 = (self.M_r(z))**2*math2.besselsFunc(m*self.bladeNumber,m*self.bladeNumber*z*self.M_t*np.sin(self.theta)/(1-self.flow.M_x*np.cos(self.theta)))
+        P_Lm_2 = 1j * k_x *C_L/2*self.psi_L(0.01,z,m)
+        P_Lm_2_alt = 1j * k_x *C_L/2*self.psi_L2(z,m)
+        return (P_Lm_1*P_Lm_2,P_Lm_1*P_Lm_2_alt)
+
+    def CLfunction(self, x):
+        Lift = 1
+        return .2
+
+    def CDfunction(self, x):
+        return .0005
 
 class Math2:
     def integration(self, step, array, type):
-        if type=='trapezium':
+        if type==0:
             area=0
             for i in range(len(array)-1):
                 area += (array[i]+ array[i+1])*step/2
             return area
-        if type== 'Simpsons':
+        if type == 1:
             area=0
             if (len(array)/2)%1==0:
                 i=0
